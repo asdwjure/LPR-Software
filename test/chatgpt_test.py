5449e5cfae88384ac26e390b34d814dfebe547e2
Jure Rebernik <jr.rebernik@gmail.com>
2023-02-28 09:27:54 +0100

    EasyOCR working (in virtual env tfod2).

--- test/chatgpt_test.py
+++ test/chatgpt_test.py
@@ -1,61 +1,13 @@
-import cv2
-import pytesseract
+# Import required libraries
+import tensorflow as tf
+from PIL import Image
 
-pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\\tesseract.exe' # Tesseract path on windows (NOTE: For Linux this must be deleted)
-
-# Set up the USB camera
-# camera = cv2.VideoCapture(0)
-
-# Load the license plate detection model
-plate_cascade = cv2.CascadeClassifier('config/haarcascade_russian_plate_number.xml')
-
-# Capture a frame from the camera
-# ret, frame = camera.read()
-frame = cv2.imread('images/slo3.jpg')
-
-# Convert the frame to grayscale for easier processing
-gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-cv2.imshow('Original slika', gray)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-# Detect license plates in the frame
-plates = plate_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4, minSize=(50, 30))
-
-# Loop through each detected license plate
-for (x, y, w, h) in plates:
-    # Crop the license plate region from the frame
-    plate_img = frame[y:y+h, x:x+w]
-
-    # Apply some image preprocessing to enhance text recognition
-    plate_img = cv2.cvtColor(plate_img, cv2.COLOR_BGR2GRAY)
-    plate_img = cv2.GaussianBlur(plate_img, (5, 5), 0)
-    plate_img = cv2.threshold(plate_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-
-    cv2.imshow('Sive tablice', plate_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-    # Use pytesseract to recognize the text in the license plate image
-    plate_text = pytesseract.image_to_string(plate_img, config='--psm 11')
-
-    # Print the recognized text to the console
-    print('License plate:', plate_text)
-
-    # Draw a rectangle around the license plate region in the frame
-    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-# Display the processed frame with license plate regions highlighted
-cv2.imshow('License Plate Recognition', frame)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-
-# # Press 'q' to quit the loop
-# if cv2.waitKey(1) & 0xFF == ord('q'):
-#     break
-
-# Release the camera and close the window
-# camera.release()
-# cv2.destroyAllWindows()
+# Read a PIL image  
+img = Image.open('/home/jrebernik/Magistrska/LPR-Software/dataset/images_resized/IMG20230220105031.jpg')
+print(img)
+# Convert the PIL image to Tensor
+img_to_tensor = tf.convert_to_tensor(img)
+# print the converted Torch tensor
+print(img_to_tensor)
+print(len(img_to_tensor))
+print("dtype of tensor:",img_to_tensor.dtype)
---
+++ test/tfod_camera_test.py
@@ -0,0 +1,109 @@
+import os
+import sys
+import time
+import tensorflow as tf
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+from object_detection.builders import model_builder
+from object_detection.utils import config_util
+import easyocr
+
+# Load pipeline config and build a detection model
+configs = config_util.get_configs_from_pipeline_file('tensorflow/workspace/models/plate_model_320/pipeline.config')
+detection_model = model_builder.build(model_config=configs['model'], is_training=False)
+
+# Restore checkpoint
+ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
+ckpt.restore(os.path.join('tensorflow/workspace/models/plate_model_320/check', 'ckpt-3')).expect_partial() # Use latest checkpoint generated
+
+@tf.function
+def detect_fn(image):
+    image, shapes = detection_model.preprocess(image)
+    prediction_dict = detection_model.predict(image, shapes)
+    detections = detection_model.postprocess(prediction_dict, shapes)
+    return detections
+
+if __name__ == '__main__':
+
+    # Limit Tensorflow GPU consumption
+    # gpus = tf.config.list_physical_devices('GPU')
+    # if gpus:
+    #     try: 
+    #         tf.config.experimental.set_virtual_device_configuration(
+    #             gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=3381)])
+    #     except:
+    #         print(sys.exc_info()[0])
+
+    category_index = label_map_util.create_category_index_from_labelmap('tensorflow/workspace/annotations/label_map.pbtxt')
+
+    cap = cv2.VideoCapture(0)
+    # width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    # height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    while cap.isOpened(): 
+    # while True:
+        ret, frame = cap.read()
+        image_np = np.array(frame)
+        
+        input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
+        detections = detect_fn(input_tensor)
+        
+        num_detections = int(detections.pop('num_detections'))
+        detections = {key: value[0, :num_detections].numpy()
+                    for key, value in detections.items()}
+        detections['num_detections'] = num_detections
+
+        # detection_classes should be ints.
+        detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+        label_id_offset = 1
+        image_np_with_detections = image_np.copy()
+
+        viz_utils.visualize_boxes_and_labels_on_image_array(
+                    image_np_with_detections,
+                    detections['detection_boxes'],
+                    detections['detection_classes']+label_id_offset,
+                    detections['detection_scores'],
+                    category_index,
+                    use_normalized_coordinates=True,
+                    max_boxes_to_draw=5,
+                    min_score_thresh=.8,
+                    agnostic_mode=False)
+        
+        cv2.imshow('object detection',  cv2.resize(image_np_with_detections, (800, 600)))
+        
+        # detection_threshold = 0.7
+        # scores = list(filter(lambda x: x> detection_threshold, detections['detection_scores']))
+        # boxes = detections['detection_boxes'][:len(scores)]
+        # classes = detections['detection_classes'][:len(scores)]
+
+        # width = image_np_with_detections.shape[1]
+        # height = image_np_with_detections.shape[0]
+
+        # # Apply ROI filtering and OCR
+        # for idx, box in enumerate(boxes):
+        #     print(box)
+        #     roi = box*[height, width, height, width]
+        #     print(roi)
+        # #     region = image_np_with_detections[int(roi[0]):int(roi[2]),int(roi[1]):int(roi[3])]
+        #     ocr_result = reader.readtext(region)
+        #     print(ocr_result)
+        #     plt.imshow(cv2.cvtColor(region, cv2.COLOR_BGR2RGB))
+
+        # for result in ocr_result:
+        #     print(np.sum(np.subtract(result[0][2],result[0][1])))
+        #     print(result[1])
+        
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            cap.release()
+            cv2.destroyAllWindows()
+            break
+
+    # reader = easyocr.Reader(['en'], gpu=False)
+    # frame = cv2.imread('dataset/images/test/IMG20230221130446.jpg', cv2.IMREAD_COLOR)
+    # result = reader.readtext(frame)
+    # print(result)
+
