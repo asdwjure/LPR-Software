a702e9d49d6163735d8a40f22915db83aa9e2ea3
Jure Rebernik <jr.rebernik@gmail.com>
2023-03-12 09:45:08 +0100

    Added python test files.

---
+++ test/ocr_tflite.py
@@ -0,0 +1,477 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+# from tflite_runtime.interpreter import Interpreter
+import cv2
+import os
+import string
+import time
+import torch
+from torch.autograd import Variable
+import math
+import pytesseract
+
+DEFAULT_ALPHABET = string.digits + string.ascii_uppercase
+alphabets = DEFAULT_ALPHABET
+blank_index = len(alphabets)
+
+
+def normalizeMeanVariance(in_img, mean=(0.485, 0.456, 0.406), variance=(0.229, 0.224, 0.225)):
+    # should be RGB order
+    img = in_img.copy().astype(np.float32)
+
+    img -= np.array([mean[0] * 255.0, mean[1] * 255.0, mean[2] * 255.0], dtype=np.float32)
+    img /= np.array([variance[0] * 255.0, variance[1] * 255.0, variance[2] * 255.0], dtype=np.float32)
+    return img
+
+def denormalizeMeanVariance(in_img, mean=(0.485, 0.456, 0.406), variance=(0.229, 0.224, 0.225)):
+    # should be RGB order
+    img = in_img.copy()
+    img *= variance
+    img += mean
+    img *= 255.0
+    img = np.clip(img, 0, 255).astype(np.uint8)
+    return img
+
+def resize_aspect_ratio(img, square_size, interpolation, mag_ratio=1):
+    height, width, channel = img.shape
+
+    # magnify image size
+    target_size = mag_ratio * max(height, width)
+
+    # set original image size
+    if target_size > square_size:
+        target_size = square_size
+    
+    ratio = target_size / max(height, width)    
+
+    target_h, target_w = int(height * ratio), int(width * ratio)
+    proc = cv2.resize(img, (target_w, target_h), interpolation = interpolation)
+
+
+    # make canvas and paste image
+    target_h32, target_w32 = target_h, target_w
+    if target_h % 32 != 0:
+        target_h32 = target_h + (32 - target_h % 32)
+    if target_w % 32 != 0:
+        target_w32 = target_w + (32 - target_w % 32)
+    resized = np.zeros((target_h32, target_w32, channel), dtype=np.float32)
+    resized[0:target_h, 0:target_w, :] = proc
+    target_h, target_w = target_h32, target_w32
+
+    size_heatmap = (int(target_w/2), int(target_h/2))
+
+    return resized, ratio, size_heatmap
+
+
+""" auxilary functions """
+# unwarp corodinates
+def warpCoord(Minv, pt):
+    out = np.matmul(Minv, (pt[0], pt[1], 1))
+    return np.array([out[0]/out[2], out[1]/out[2]])
+""" end of auxilary functions """
+
+
+def getDetBoxes_core(textmap, linkmap, text_threshold, link_threshold, low_text):
+    # prepare data
+    linkmap = linkmap.copy()
+    textmap = textmap.copy()
+    img_h, img_w = textmap.shape
+
+    """ labeling method """
+    ret, text_score = cv2.threshold(textmap, low_text, 1, 0)
+    ret, link_score = cv2.threshold(linkmap, link_threshold, 1, 0)
+
+    text_score_comb = np.clip(text_score + link_score, 0, 1)
+    nLabels, labels, stats, centroids = cv2.connectedComponentsWithStats(text_score_comb.astype(np.uint8), connectivity=4)
+
+    det = []
+    mapper = []
+    for k in range(1,nLabels):
+        # size filtering
+        size = stats[k, cv2.CC_STAT_AREA]
+        if size < 10: continue
+
+        # thresholding
+        if np.max(textmap[labels==k]) < text_threshold: continue
+
+        # make segmentation map
+        segmap = np.zeros(textmap.shape, dtype=np.uint8)
+        segmap[labels==k] = 255
+        segmap[np.logical_and(link_score==1, text_score==0)] = 0   # remove link area
+        x, y = stats[k, cv2.CC_STAT_LEFT], stats[k, cv2.CC_STAT_TOP]
+        w, h = stats[k, cv2.CC_STAT_WIDTH], stats[k, cv2.CC_STAT_HEIGHT]
+        niter = int(math.sqrt(size * min(w, h) / (w * h)) * 2)
+        sx, ex, sy, ey = x - niter, x + w + niter + 1, y - niter, y + h + niter + 1
+        # boundary check
+        if sx < 0 : sx = 0
+        if sy < 0 : sy = 0
+        if ex >= img_w: ex = img_w
+        if ey >= img_h: ey = img_h
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(1 + niter, 1 + niter))
+        segmap[sy:ey, sx:ex] = cv2.dilate(segmap[sy:ey, sx:ex], kernel)
+
+        # make box
+        np_contours = np.roll(np.array(np.where(segmap!=0)),1,axis=0).transpose().reshape(-1,2)
+        rectangle = cv2.minAreaRect(np_contours)
+        box = cv2.boxPoints(rectangle)
+
+        # align diamond-shape
+        w, h = np.linalg.norm(box[0] - box[1]), np.linalg.norm(box[1] - box[2])
+        box_ratio = max(w, h) / (min(w, h) + 1e-5)
+        if abs(1 - box_ratio) <= 0.1:
+            l, r = min(np_contours[:,0]), max(np_contours[:,0])
+            t, b = min(np_contours[:,1]), max(np_contours[:,1])
+            box = np.array([[l, t], [r, t], [r, b], [l, b]], dtype=np.float32)
+
+        # make clock-wise order
+        startidx = box.sum(axis=1).argmin()
+        box = np.roll(box, 4-startidx, 0)
+        box = np.array(box)
+
+        det.append(box)
+        mapper.append(k)
+
+    return det, labels, mapper
+
+def getPoly_core(boxes, labels, mapper, linkmap):
+    # configs
+    num_cp = 5
+    max_len_ratio = 0.7
+    expand_ratio = 1.45
+    max_r = 2.0
+    step_r = 0.2
+
+    polys = []  
+    for k, box in enumerate(boxes):
+        # size filter for small instance
+        w, h = int(np.linalg.norm(box[0] - box[1]) + 1), int(np.linalg.norm(box[1] - box[2]) + 1)
+        if w < 10 or h < 10:
+            polys.append(None); continue
+
+        # warp image
+        tar = np.float32([[0,0],[w,0],[w,h],[0,h]])
+        M = cv2.getPerspectiveTransform(box, tar)
+        word_label = cv2.warpPerspective(labels, M, (w, h), flags=cv2.INTER_NEAREST)
+        try:
+            Minv = np.linalg.inv(M)
+        except:
+            polys.append(None); continue
+
+        # binarization for selected label
+        cur_label = mapper[k]
+        word_label[word_label != cur_label] = 0
+        word_label[word_label > 0] = 1
+
+        """ Polygon generation """
+        # find top/bottom contours
+        cp = []
+        max_len = -1
+        for i in range(w):
+            region = np.where(word_label[:,i] != 0)[0]
+            if len(region) < 2 : continue
+            cp.append((i, region[0], region[-1]))
+            length = region[-1] - region[0] + 1
+            if length > max_len: max_len = length
+
+        # pass if max_len is similar to h
+        if h * max_len_ratio < max_len:
+            polys.append(None); continue
+
+        # get pivot points with fixed length
+        tot_seg = num_cp * 2 + 1
+        seg_w = w / tot_seg     # segment width
+        pp = [None] * num_cp    # init pivot points
+        cp_section = [[0, 0]] * tot_seg
+        seg_height = [0] * num_cp
+        seg_num = 0
+        num_sec = 0
+        prev_h = -1
+        for i in range(0,len(cp)):
+            (x, sy, ey) = cp[i]
+            if (seg_num + 1) * seg_w <= x and seg_num <= tot_seg:
+                # average previous segment
+                if num_sec == 0: break
+                cp_section[seg_num] = [cp_section[seg_num][0] / num_sec, cp_section[seg_num][1] / num_sec]
+                num_sec = 0
+
+                # reset variables
+                seg_num += 1
+                prev_h = -1
+
+            # accumulate center points
+            cy = (sy + ey) * 0.5
+            cur_h = ey - sy + 1
+            cp_section[seg_num] = [cp_section[seg_num][0] + x, cp_section[seg_num][1] + cy]
+            num_sec += 1
+
+            if seg_num % 2 == 0: continue # No polygon area
+
+            if prev_h < cur_h:
+                pp[int((seg_num - 1)/2)] = (x, cy)
+                seg_height[int((seg_num - 1)/2)] = cur_h
+                prev_h = cur_h
+
+        # processing last segment
+        if num_sec != 0:
+            cp_section[-1] = [cp_section[-1][0] / num_sec, cp_section[-1][1] / num_sec]
+
+        # pass if num of pivots is not sufficient or segment widh is smaller than character height 
+        if None in pp or seg_w < np.max(seg_height) * 0.25:
+            polys.append(None); continue
+
+        # calc median maximum of pivot points
+        half_char_h = np.median(seg_height) * expand_ratio / 2
+
+        # calc gradiant and apply to make horizontal pivots
+        new_pp = []
+        for i, (x, cy) in enumerate(pp):
+            dx = cp_section[i * 2 + 2][0] - cp_section[i * 2][0]
+            dy = cp_section[i * 2 + 2][1] - cp_section[i * 2][1]
+            if dx == 0:     # gradient if zero
+                new_pp.append([x, cy - half_char_h, x, cy + half_char_h])
+                continue
+            rad = - math.atan2(dy, dx)
+            c, s = half_char_h * math.cos(rad), half_char_h * math.sin(rad)
+            new_pp.append([x - s, cy - c, x + s, cy + c])
+
+        # get edge points to cover character heatmaps
+        isSppFound, isEppFound = False, False
+        grad_s = (pp[1][1] - pp[0][1]) / (pp[1][0] - pp[0][0]) + (pp[2][1] - pp[1][1]) / (pp[2][0] - pp[1][0])
+        grad_e = (pp[-2][1] - pp[-1][1]) / (pp[-2][0] - pp[-1][0]) + (pp[-3][1] - pp[-2][1]) / (pp[-3][0] - pp[-2][0])
+        for r in np.arange(0.5, max_r, step_r):
+            dx = 2 * half_char_h * r
+            if not isSppFound:
+                line_img = np.zeros(word_label.shape, dtype=np.uint8)
+                dy = grad_s * dx
+                p = np.array(new_pp[0]) - np.array([dx, dy, dx, dy])
+                cv2.line(line_img, (int(p[0]), int(p[1])), (int(p[2]), int(p[3])), 1, thickness=1)
+                if np.sum(np.logical_and(word_label, line_img)) == 0 or r + 2 * step_r >= max_r:
+                    spp = p
+                    isSppFound = True
+            if not isEppFound:
+                line_img = np.zeros(word_label.shape, dtype=np.uint8)
+                dy = grad_e * dx
+                p = np.array(new_pp[-1]) + np.array([dx, dy, dx, dy])
+                cv2.line(line_img, (int(p[0]), int(p[1])), (int(p[2]), int(p[3])), 1, thickness=1)
+                if np.sum(np.logical_and(word_label, line_img)) == 0 or r + 2 * step_r >= max_r:
+                    epp = p
+                    isEppFound = True
+            if isSppFound and isEppFound:
+                break
+
+        # pass if boundary of polygon is not found
+        if not (isSppFound and isEppFound):
+            polys.append(None); continue
+
+        # make final polygon
+        poly = []
+        poly.append(warpCoord(Minv, (spp[0], spp[1])))
+        for p in new_pp:
+            poly.append(warpCoord(Minv, (p[0], p[1])))
+        poly.append(warpCoord(Minv, (epp[0], epp[1])))
+        poly.append(warpCoord(Minv, (epp[2], epp[3])))
+        for p in reversed(new_pp):
+            poly.append(warpCoord(Minv, (p[2], p[3])))
+        poly.append(warpCoord(Minv, (spp[2], spp[3])))
+
+        # add to final result
+        polys.append(np.array(poly))
+
+    return polys
+
+def getDetBoxes(textmap, linkmap, text_threshold, link_threshold, low_text, poly=False):
+    boxes, labels, mapper = getDetBoxes_core(textmap, linkmap, text_threshold, link_threshold, low_text)
+
+    if poly:
+        polys = getPoly_core(boxes, labels, mapper, linkmap)
+    else:
+        polys = [None] * len(boxes)
+
+    return boxes, polys
+
+def adjustResultCoordinates(polys, ratio_w, ratio_h, ratio_net = 2):
+    if len(polys) > 0:
+        polys = np.array(polys)
+        for k in range(len(polys)):
+            if polys[k] is not None:
+                polys[k] *= (ratio_w * ratio_net, ratio_h * ratio_net)
+    return polys
+
+
+
+def run_tflite_detection_model(input_data):
+    # Load the TFLite model and allocate tensors.
+    interpreter = tf.lite.Interpreter(model_path=f"test/detect_dr.tflite")
+    interpreter.allocate_tensors()
+
+    # Get input and output tensors.
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+ 
+    # Test the model on random input data.
+    input_shape = input_details[0]['shape']
+    # input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+
+    interpreter.invoke()
+
+    # The function `get_tensor()` returns a copy of the tensor data.
+    # Use `tensor()` in order to get a pointer to the tensor.
+    y = interpreter.get_tensor(output_details[0]['index'])
+    feature = interpreter.get_tensor(output_details[1]['index'])
+
+    return y, feature
+
+
+
+def run_tflite_recognition_model(image, quantization):
+    # input_data = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    input_data = image
+    input_data = cv2.resize(input_data, (200, 31))
+    input_data = input_data[np.newaxis]
+    input_data = np.expand_dims(input_data, 3)
+    input_data = input_data.astype('float32')/255
+    path = f'test/ocr_{quantization}.tflite'
+    interpreter = tf.lite.Interpreter(model_path=path)
+    interpreter.allocate_tensors()
+
+    # Get input and output tensors.
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    input_shape = input_details[0]['shape']
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+
+    interpreter.invoke()
+
+    output = interpreter.get_tensor(output_details[0]['index'])
+    return output
+
+def cvt2HeatmapImg(img):
+    img = (np.clip(img, 0, 1) * 255).astype(np.uint8)
+    img = cv2.applyColorMap(img, cv2.COLORMAP_JET)
+    return img
+
+def saveResult(img_file, img, boxes, dirname='./result/', verticals=None, texts=None):
+        """ save text detection result one by one
+        Args:
+            img_file (str): image file name
+            img (array): raw image context
+            boxes (array): array of result file
+                Shape: [num_detections, 4] for BB output / [num_detections, 4] for QUAD output
+        Return:
+            None
+        """
+        img = np.array(img)
+
+        # make result file list
+        filename, file_ext = os.path.splitext(os.path.basename(img_file))
+
+        # result directory
+        res_file = dirname + "res_" + filename + '.txt'
+        res_img_file = dirname + "res_" + filename + '.jpg'
+
+        if not os.path.isdir(dirname):
+            os.mkdir(dirname)
+        #data = open('task3.txt', 'w')
+        count = 0
+        with open(res_file, 'w') as f:
+            for i, box in enumerate(boxes):
+                #text = save_polygon(img, box, count)
+                #box_data = ""
+                #for co_ord in box:
+                #    box_data+=f"{co_ord[0]}, {co_ord[1]}"
+                #print(box_data, text)
+                #data.write(box_data+","+text+"\n")
+                #count+=1
+                poly = np.array(box).astype(np.int32).reshape((-1))
+                #strResult = ','.join([str(p) for p in poly]) + '\r\n'
+                #f.write(strResult)
+                poly = poly.reshape(-1, 2)
+                min_co = tuple(np.min(poly, axis=0))
+                max_co = tuple(np.max(poly, axis=0))
+                #x_1, x_2, y_1, y_2 = poly[0][0], poly[1][0], poly[1][1], poly[2][1]
+                cv2.rectangle(img, min_co, max_co, (0, 0, 255), 2)
+                #cv2.polylines(img, [poly.reshape((-1, 1, 2))], True, color=(0, 0, 255), thickness=2)
+                ptColor = (0, 255, 255)
+                if verticals is not None:
+                    if verticals[i]:
+                        ptColor = (255, 0, 0)
+
+                if texts is not None:
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    font_scale = 0.5
+                    cv2.putText(img, "{}".format(texts[i]), (poly[0][0]+1, poly[0][1]+1), font, font_scale, (0, 0, 0), thickness=1)
+                    cv2.putText(img, "{}".format(texts[i]), tuple(poly[0]), font, font_scale, (0, 255, 255), thickness=1)
+
+        # Save result image
+        cv2.imwrite(res_img_file, img)
+
+if __name__ == '__main__':
+    img = cv2.imread('dataset/images/train/IMG20230220132839.jpg')
+    # img = cv2.convertScaleAbs(img, alpha=0.05, beta=200)
+    # img = cv2.GaussianBlur(img, (7,7), 0)
+    # cv2.imshow('img', img)
+    plate = img[445:531, 521:921].copy()
+    plate[:,0:55] = 255
+    plate[20:70,140:180] = 255
+    
+    # plate = cv2.resize(plate, dsize=(600, 800), interpolation=cv2.INTER_LINEAR)
+    # img_resized, target_ratio, size_heatmap = resize_aspect_ratio(plate, 800, interpolation=cv2.INTER_LINEAR, mag_ratio=1)
+    # ratio_h = ratio_w = 1 / target_ratio
+
+    # # preprocessing
+    # x = normalizeMeanVariance(plate)
+    # x = torch.from_numpy(x).permute(2, 0, 1)    # [h, w, c] to [c, h, w]
+    # x = Variable(x.unsqueeze(0))                # [c, h, w] to [b, c, h, w]
+    # # forward pass
+
+    # x = x.cpu().detach().numpy()
+    # y, feature = run_tflite_detection_model(x)
+
+    # y = torch.from_numpy(y)
+    # feature = torch.from_numpy(feature)
+    # # make score and link map
+    # score_text = y[0,:,:,0].cpu().data.numpy()
+    # score_link = y[0,:,:,1].cpu().data.numpy()
+
+    # text_threshold = 0.7
+    # link_threshold = 0.4
+    # low_text = 0.4
+    # poly = False
+
+    # # Post-processing
+    # boxes, polys = getDetBoxes(score_text, score_link, text_threshold, link_threshold, low_text, poly)
+
+    # # coordinate adjustment
+    # boxes = adjustResultCoordinates(boxes, ratio_w, ratio_h)
+    # polys = adjustResultCoordinates(polys, ratio_w, ratio_h)
+    # for k in range(len(polys)):
+    #     if polys[k] is None: polys[k] = boxes[k]
+
+    # # render results (optional)
+    # render_img = score_text.copy()
+    # render_img = np.hstack((render_img, score_link))
+    # ret_score_text = cvt2HeatmapImg(render_img)
+
+    # saveResult('test/', plate[:,:,::-1], polys, dirname='output')
+
+    # Running Dynamic Range Quantization
+    plate = np.mean(plate, axis=2)
+    _, plate = cv2.threshold(plate, 100, 255, cv2.THRESH_BINARY)
+    cv2.imshow('plate', plate)
+    t1 = time.time()
+    tflite_output = run_tflite_recognition_model(plate, 'dr')
+    t2 = time.time()
+    print('Time (ms):', (t2-t1)*1000)
+    final_output = "".join(alphabets[index] for index in tflite_output[0] if index not in [blank_index, -1])
+    print(final_output)
+
+    # tess_ocr = pytesseract.image_to_string(plate)
+    # print(tess_ocr)
+
+    cv2.imshow('image', img)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
--- test/opencv_test.py
+++ test/opencv_test.py
@@ -1,56 +1,117 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
+import time
+
+
+def gammaCorrection(img, desired_mean_out):
+    mean = np.mean(img)
+    gamma = np.log(desired_mean_out/255)/np.log(mean/255)
+    img_gamma = np.power(img/255.0, gamma)
+    img_gamma = np.uint8(img_gamma*255)
+    return gamma, img_gamma
+
+def plate2chars(img, debug=False):
+    num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(img)
+
+    # initialize an output mask to store all characters parsed from the license plate
+    mask = np.zeros(img.shape, dtype="uint8")
+
+    # loop over the number of unique connected component labels
+    for i in range(0, num_labels):
+        # extract the connected component statistics and centroid for the current label
+        x = stats[i, cv2.CC_STAT_LEFT]
+        y = stats[i, cv2.CC_STAT_TOP]
+        w = stats[i, cv2.CC_STAT_WIDTH]
+        h = stats[i, cv2.CC_STAT_HEIGHT]
+        area = stats[i, cv2.CC_STAT_AREA]
+        (cX, cY) = centroids[i]
+        
+        # Only grab components of the appropriate width and height
+        keepWidth = w > 23 and w < 41 # 28-36 plus 5 pixels of margain
+        keepHeight = h > 51 and h < 64 # 56-59 plus 5 pixels of margain
+        # keepArea = area > 500 and area < 1500
+
+        if all((keepWidth, keepHeight)):
+            # construct a mask for the current connected component and then take the bitwise OR with the mask
+            componentMask = (labels == i).astype("uint8") * 255
+            mask = cv2.bitwise_or(mask, componentMask)
+        
+        if debug:
+        # clone our original image (so we can draw on it) and then draw
+        # a bounding box surrounding the connected component along with
+        # a circle corresponding to the centroid
+            output = img.copy()
+            cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 3)
+            cv2.circle(output, (int(cX), int(cY)), 4, (0, 0, 255), -1)
+            
+            # construct a mask for the current connected component by
+            # finding a pixels in the labels array that have the current
+            # connected component ID
+            componentMask = (labels == i).astype("uint8") * 255
+
+            cv2.imshow("Connected Component", componentMask)
+            cv2.waitKey(0)
+
+    return mask
+
 
 
 kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
 
 
 
-img = cv2.imread('dataset/images/test/IMG20230220132908.jpg')
-plate = img[399:523, 360:630]
-test = plate[50:60, 0:200]
-print(np.min(test))
-cv2.imshow('test', test)
-gray = cv2.cvtColor(plate, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (7,7), 0)
+img = cv2.imread('dataset/images/train/IMG20230220132839.jpg')
+# img = cv2.convertScaleAbs(img, alpha=0.8, beta=-20)
+# img = np.power(img/255, 0.8)
+# img = (img*255).astype(np.uint8)
 
-eq = cv2.equalizeHist(gray)
+# Add noise to image
+noise = np.random.normal(0, 25, img.shape)
+img = np.clip(img + noise, 0, 255).astype(np.uint8)
+# img = cv2.GaussianBlur(img, (15,15), 0)
+cv2.imshow('img', img)
 
-cv2.imshow('Original', gray)
-cv2.imshow('Eq', eq)
+plate = img[440:541, 516:926]
+plate = cv2.GaussianBlur(plate, (5,5), 0)
+plate = cv2.resize(plate, (410,100), interpolation=cv2.INTER_LINEAR) # Ta interpolacija proizvede shitty sliko
 
-min_value = np.min(eq)
-mean_value = np.mean(eq)
-mean_gray = np.mean(gray)
-print(min_value, mean_value, mean_gray)
-_, binary = cv2.threshold(eq, mean_value-50, 255, cv2.THRESH_BINARY)
-cv2.imshow('thr img', binary)
-binary = cv2.erode(binary, kernel=kernel)
-# binary = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel)
-cv2.imshow('eroded', binary)
+gray = np.mean(plate, axis=2).astype(np.uint8) # Raje dej tako v crno belo
+gray_mean = np.mean(gray)
+cv2.imshow('gray plate', gray)
+print('Mean value of gray plate:', gray_mean)
 
-# binary = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 15)
-# _, binary = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-# cv2.imshow('thr img', binary)
+# Apply gamma correction to the image
+gamma, img_gamma = gammaCorrection(gray, 110)
+plate = cv2.GaussianBlur(plate, (3,3), 0)
+cv2.imshow('Gamma corrected plate', img_gamma)
+print('Gamma factor =', gamma)
+
+# laplacian_var = cv2.Laplacian(gray, cv2.CV_64F).var()
+
+histogram_gray = cv2.calcHist([gray], [0], None, [256], [0,256], accumulate=False)
+histogram_gamma = cv2.calcHist([img_gamma], [0], None, [256], [0,256], accumulate=False)
+
+# _, img_thr = cv2.threshold(img_gamma.copy(), 60, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+img_thr = img_gamma.copy()
+img_thr = cv2.adaptiveThreshold(img_thr, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 31, 10)
+# kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+# img_thr = cv2.morphologyEx(img_thr, cv2.MORPH_OPEN, kernel)
+# img_thr = cv2.morphologyEx(img_thr, cv2.MORPH_CLOSE, kernel)
+cv2.imshow('img thr', img_thr)
+
+plate_chars = plate2chars(img_thr)
+plate_chars = cv2.bitwise_not(plate_chars)
+cv2.imshow("Characters", plate_chars)
 
 
-# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# cv2.imshow('gray after thr', gray)
-# inverted = cv2.bitwise_not(binary)
-# cv2.imshow('mask', inverted)
-# masked = cv2.bitwise_and(img, img, mask=inverted)
-# cv2.imshow('Masked img', masked)
 
-# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# gray = cv2.GaussianBlur(gray, (7,7), 0)
-
-# gray = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-# cv2.imshow('Gray threshold', gray)
-
-# img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
-
-# plate = gray[399:523, 360:630]
-# cv2.imshow('Plate', plate)
+plt.figure(1)
+plt.subplot(211)
+plt.plot(histogram_gray)
+plt.subplot(212)
+plt.plot(histogram_gamma)
+plt.show()
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
